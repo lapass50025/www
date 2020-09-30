51b8215fca2c3257961fb67eec3f2f68c40fc282
lapass50025 <lapass50025@gmail.com>
2020-09-30 17:56:23 +0900

    list page and question_list.html added

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from pybo.models import Question, Answer
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!!!")
+    question_list = Question.objects.order_by('-create_date')
+    context = { 'question_list': question_list }
+    return render(request, 'pybo/question_list.html', context)
