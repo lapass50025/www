6243c38cf5a921441039063f324852539f659c0e
lapass50025 <lapass50025@gmail.com>
2020-10-03 16:19:43 +0900

    join added : login, logout added

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'common'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='common_login'),
     path('logout/', auth_views.LogoutView.as_view(), name='common_logout'),
+
+    path('signup/', views.signup, name='common_signup'),
 ]
